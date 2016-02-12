9bc409269e3871f38c83dafa3de0f151e39ddb21
Tatsuya Iizuka <iizuka.tatsu@gmail.com>
2016-02-12 17:25:19 +0900

    conflicted

--- js/simulation.js
+++ js/simulation.js
@@ -186,7 +186,11 @@ Model.prototype.calculate = function(){
 	var i=0;
 	var info = [];
 	drive_time_task_list = [0,0,0];
+<<<<<<< Updated upstream
 	while(this.S > 0){
+=======
+	while(this.S > 0 && i < 15000){
+>>>>>>> Stashed changes
 		next_time = this.setNextTime(now_time);
 		this.Ph = this.getHarvestedPower(now_time);
 		this.Pstandby = this.getStandbyPower();
